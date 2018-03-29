848e85f0bc9f45d3ed50ba1cbea69de47bff3d54
XueCong <xuecong98@gmail.com>
2018-03-29 01:27:02 +0000

    api/user

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,8 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function($api) {
     $api->group([
         'middleware' => 'api.throttle',
@@ -45,5 +46,11 @@ $api->version('v1', [
         $api->delete('authorizations/current', 'AuthorizationsController@destory')
             ->name('api.authorizations.destory');
 
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
     });
 });
