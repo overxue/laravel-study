e77d8235b3748d5e3bad00d1fab97e9e461c42dc
XueCong <xuecong98@gmail.com>
2018-04-18 08:11:14 +0000

    Users can edit their own topic

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,7 +11,8 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
-        $topic->body = clean($topic->body, 'user_topic_body');
+//        $topic->body = clean($topic->body, 'user_topic_body');
+        $topic->body = clean(htmlspecialchars_decode($topic->body), 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
