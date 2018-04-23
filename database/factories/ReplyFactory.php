93b036c3ee73f7ffe95ee4475260c251ec73355e
XueCong <xuecong98@gmail.com>
2018-04-23 07:43:33 +0000

    List user replies

---
+++ database/factories/ReplyFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Reply::class, function (Faker $faker) {
+    // 随机获取一个月类的时间
+    $time = $faker->dateTimeThisMonth();
+
+    return [
+        'content' => $faker->sentence(),
+        'created_at' => $time,
+        'updated_at' => $time,
+    ];
+});
