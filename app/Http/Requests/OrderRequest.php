059aa5cb8db7366be513f311a7cf695ba03789f5
XueCong <xuecong98@gmail.com>
2019-03-06 10:26:41 +0800

    last commit

---
+++ app/Http/Requests/OrderRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+use App\Models\ProductSku;
+
+class OrderRequest extends Request
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
