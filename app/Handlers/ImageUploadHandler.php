aac1cef451294f56c1f67760c60170ea9a4bd562
XueCong <xuecong98@gmail.com>
2019-03-18 22:27:08 +0800

    resize avatar

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -1,12 +1,14 @@
 <?php
 namespace App\Handlers;
 
+use Image;
+
 class ImageUploadHandler
 {
     // 只允许以下后缀的图片文件上传
     protected $allowed_ext = ['png', 'jpg', 'gif', 'jpeg'];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         $folder_name = "uploads/images/$folder/" . date('Ym/d', time());
         $upload_path = public_path() . '/' . $folder_name;
@@ -21,8 +23,29 @@ class ImageUploadHandler
         // 将图片移动到目标路径存储
         $file->move($upload_path, $filename);
 
+        // 如果限制了图片的宽度，就进行剪裁
+        if ($max_width && $extension != 'gif') {
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
         return [
             'path' => config('app.url') . "/$folder_name/$filename"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        $image = Image::make($file_path);
+
+        $image->resize($max_width, null, function ($constraint) {
+            // 设定宽度是 $max_width，高度等比例双方缩放
+            $constraint->aspectRatio();
+
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        // 对图片修改后进行保存
+        $image->save();
     }
 }
