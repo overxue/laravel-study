eb4690914bc8a6c08d641b2a4333de7b57683b5f
XueCong <xuecong98@gmail.com>
2019-03-19 21:39:06 +0800

    user and topic

--- database/seeds/TopicsTableSeeder.php
+++ database/seeds/TopicsTableSeeder.php
@@ -7,10 +7,18 @@ class TopicsTableSeeder extends Seeder
 {
     public function run()
     {
-        $topics = factory(Topic::class)->times(50)->make()->each(function ($topic, $index) {
-            if ($index == 0) {
-                // $topic->field = 'value';
-            }
+        // 所有用户 ID 数组
+        $user_ids = \App\Models\User::all()->pluck('id')->toArray();
+
+        // 所有分类 ID 数组
+        $category_ids = \App\Models\Category::all()->pluck('id')->toArray();
+
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)->times(1000)->make()->each(function ($topic, $index) use ($user_ids, $category_ids, $faker) {
+            $topic->user_id = $faker->randomElement($user_ids);
+            $topic->category_id = $faker->randomElement($category_ids);
         });
 
         Topic::insert($topics->toArray());
