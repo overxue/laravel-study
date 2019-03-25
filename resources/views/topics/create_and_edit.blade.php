4109defe1611fda3300a825ab1b5473b734c937d
XueCong <xuecong98@gmail.com>
2019-03-25 20:03:11 +0800

    WYSIWYG editor

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 
+@section('stylus')
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@endsection
 @section('content')
 
   <div class="container">
@@ -50,9 +53,24 @@
                     <button type="submit" class="btn btn-primary"><i class="far fa-save mr-2" aria-hidden="true"></i> 保存</button>
                   </div>
                 </form>
+            </form>
         </div>
       </div>
     </div>
   </div>
-
 @endsection
+
+@section('scripts')
+  <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+
+  <script>
+    $(document).ready(function() {
+      var editor = new Simditor({
+        textarea: $('#editor'),
+      });
+    });
+  </script>
+@stop
