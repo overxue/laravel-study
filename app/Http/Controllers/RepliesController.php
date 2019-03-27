1348cbf2d2764ff3c2aaf6ead262ff533bb2636d
XueCong <xuecong98@gmail.com>
2019-03-27 22:21:08 +0800

    notification

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -16,9 +16,9 @@ class RepliesController extends Controller
 
 	public function store(ReplyRequest $request, Reply $reply)
 	{
-		$reply->content = $request->content;
+		$reply->content = $request->input('content');
 		$reply->user_id = \Auth::id();
-        $reply->topic_id = $request->topic_id;
+        $reply->topic_id = $request->input('topic_id');
         $reply->save();
         return redirect()->to($reply->topic->link())->with('success', '评论创建成功！');
 	}
