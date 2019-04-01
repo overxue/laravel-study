ac6e458c56a4bb7c27a29d1a5cfb64cf5829feaf
XueCong <xuecong98@gmail.com>
2019-04-01 23:28:10 +0800

    user can register using phone

---
+++ app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\UserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function store(UserRequest $request, User $user)
+    {
+        $verifyData = \Cache::get($request->input('verification_key'));
+
+        if (!$verifyData) {
+            return $this->response->error('验证码已失效', 422);
+        }
+
+        if (!hash_equals($verifyData['code'], $request->input('verification_code'))) {
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        $user->fill($request->all());
+        $user->phone = $verifyData['phone'];
+        $user->password = bcrypt($request->input('password'));
+        $user->save();
+
+        // 清除验证码缓存
+        \Cache::forget($request->input('verification_key'));
+
+        return $this->response->created();
+    }
+}
