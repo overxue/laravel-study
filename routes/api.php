a2dcd0e98438d424c8c98bb6496872c7c8519eeb
XueCong <xuecong98@gmail.com>
2019-04-14 20:47:47 +0800

    topics store

--- routes/api.php
+++ routes/api.php
@@ -44,6 +44,8 @@ $api->version('v1', [
             $api->patch('user', 'UsersController@update');
             // 图片资源
             $api->post('images', 'ImagesController@store');
+            // 发布话题
+            $api->post('topics', 'TopicsController@store');
         });
     });
 });
