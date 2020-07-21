c754d7a8ba1fa6305563adbb2ccf66b86887c647
XueCong <xuecong98@gmail.com>
2020-07-21 09:36:17 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,10 +8,15 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
-    	$topics = Topic::where('category_id', $category->id)->paginate(20);
+        // 读取分类 ID 关联的话题，并按每 20 条分页
+        $topics = $topic->withOrder($request->order)
+                        ->where('category_id', $category->id)
+                        ->with('user', 'category')   // 预加载防止 N+1 问题
+                        ->paginate(20);
 
-    	return view('topics.index', compact('topics', 'category'));
+        // 传参变量话题和分类到模板中
+        return view('topics.index', compact('topics', 'category'));
     }
 }
