9db328dd084e4cb79db143e9cba4bc30ca925c74
XueCong <xuecong98@gmail.com>
2020-08-04 01:33:23 +0000

    发送短信验证码

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::prefix('v1')->name('api.v1.')->group(function() {
-    
+Route::prefix('v1')->name('api.v1.')->group(function () {
+    // 短信验证码
+    Route::post('verificationCodes', 'api\VerificationCodesController@store')
+        ->name('verificationCodes.store');
 });
