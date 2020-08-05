9c11b5d677b26cdeac637735f9b76a230659125e
XueCong <xuecong98@gmail.com>
2020-08-05 00:58:34 +0000

    图片验证码

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,8 @@ Route::prefix('v1')
 
         Route::middleware('throttle:' . config('api.rate_limits.sign'))
             ->group(function () {
+            	// 图片验证码
+            	Route::post('captchas', 'CaptchasController@store')->name('captchas.store');
                 // 短信验证码
                 Route::post('verificationCodes', 'VerificationCodesController@store')
                     ->name('verificationCodes.store');
@@ -24,4 +26,3 @@ Route::prefix('v1')
     });
 
 
-                   
