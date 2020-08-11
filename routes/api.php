55c6d230a93527afbe38a3675d11780c38955a19
XueCong <xuecong98@gmail.com>
2020-08-11 08:03:42 +0000

    推荐资源列表

--- routes/api.php
+++ routes/api.php
@@ -46,6 +46,8 @@ Route::prefix('v1')
                 // 某个用户发布的话题
                 Route::get('users/{user}/topics', 'TopicsController@userIndex')
                     ->name('users.topics.index');
+                // 资源推荐
+                Route::get('links', 'LinksController@index')->name('links.index');
 
                 // 登录后可以访问的接口
                 Route::middleware('auth:api')->group(function() {
