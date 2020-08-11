5f4ef7645ee03814336aa7140a05dded2c334d58
XueCong <xuecong98@gmail.com>
2020-08-11 01:33:58 +0000

    回复列表

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,12 @@ Route::prefix('v1')
                 Route::get('categories', 'CategoriesController@index')->name('categories.index');
                 // 话题列表，详情
                 Route::resource('topics', 'TopicsController')->only(['index', 'show']);
+                // 话题回复列表
+                Route::get('topics/{topic}/replies', 'RepliesController@index')
+                    ->name('topics.replies.index');
+                    // 某个用户的回复列表
+                Route::get('users/{user}/replies', 'RepliesController@userIndex')
+                    ->name('users.replies.index');
                 // 某个用户发布的话题
                 Route::get('users/{user}/topics', 'TopicsController@userIndex')
                     ->name('users.topics.index');
