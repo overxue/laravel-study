b9724cc9968832782bc5f47ee32688361ca9e4ac
XueCong <xuecong98@gmail.com>
2021-01-11 01:23:26 +0000

    回复数据

--- database/seeders/RepliesTableSeeder.php
+++ database/seeders/RepliesTableSeeder.php
@@ -3,14 +3,13 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Topic;
 use App\Models\Reply;
 
-class TopicsTableSeeder extends Seeder
+class RepliesTableSeeder extends Seeder
 {
     public function run()
     {
-        Reply::factory()->times(1000)->create();
+        Reply::factory()->count(10)->create();
     }
 }
 
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,7 +15,7 @@ class UsersTableSeeder extends Seeder
         // 单独处理第一个用户的数据
         $user = User::find(1);
         $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->email = 'xuecong98@gmail.com';
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
     }
