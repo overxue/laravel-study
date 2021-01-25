0feed9e4b0c1eed52311be8d68ce5dee8730039d
XueCong <xuecong98@gmail.com>
2021-01-25 00:55:07 +0000

    发送短信验证码

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::prefix('v1')->name('api.v1.')->group(function() {
-    
+Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
+    // 发送短信验证码
+    Route::post('verificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
 });
