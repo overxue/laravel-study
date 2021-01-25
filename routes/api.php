62a5a17dc1d7c722f82db091ad4efca287a2ef23
XueCong <xuecong98@gmail.com>
2021-01-25 08:33:23 +0000

    微信登录

--- routes/api.php
+++ routes/api.php
@@ -33,3 +33,12 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
 
             });
 });
+
+
+// https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx2052d5acb634bcb0&redirect_uri=http://larabbs.test&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect
+
+// 091qjEkl2MVxo648KUnl2HPxwX1qjEkh
+
+// https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx2052d5acb634bcb0&secret=0bcd26d42f1dcd6760d9a9475829af09&code=091qjEkl2MVxo648KUnl2HPxwX1qjEkh&grant_type=authorization_code
+
+// https://api.weixin.qq.com/sns/userinfo?access_token=41_ohdpU-bU55u2HB9S73jC7uIJyUd7kqFjK0K50o7W2WGFp3r-dxbunuK1WAoN3jdyYJxhDB8CXJNCs5MS9qwP1A&openid=odTyHwR3WqZlE-nA2pKGM6DCsD_c&lang=zh_CN
