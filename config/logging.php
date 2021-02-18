3d3d8dde4648c026cfe13254c0832c5ec4170f04
XueCong <xuecong98@gmail.com>
2021-02-18 01:12:36 +0000

    话题列表

--- config/logging.php
+++ config/logging.php
@@ -101,4 +101,11 @@ return [
         ],
     ],
 
+    'query' => [
+        'enabled' => env('LOG_QUERY', env('APP_ENV') === 'local'),
+        // Only record queries that are slower than the following time
+        // Unit: milliseconds
+        'slower_than' => 0, 
+    ],
+
 ];
